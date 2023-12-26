4e302896a76c7da14d2d25ec43b71abe0499bff0
Wonjae Choi <choi@wonj.me>
2023-12-27 07:46:07 +0900

    feat: Update DoldrumGateway

--- src/Doldrums/perpdex/DoldrumGateway.sol
+++ src/Doldrums/perpdex/DoldrumGateway.sol
@@ -2,11 +2,15 @@
 pragma solidity ^0.8.20;
 
 import {Vault} from "../vault/Vault.sol";
+import {LzApp} from "@layerzerolabs/solidity-examples/contracts/lzApp/LzApp.sol";
 import {IOFTV2} from "@layerzerolabs/solidity-examples/contracts/token/oft/v2/interfaces/IOFTV2.sol";
+import {ICommonOFT} from "@layerzerolabs/solidity-examples/contracts/token/oft/v2/interfaces/ICommonOFT.sol";
 import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract PerpDexEntrypoint {
+contract PerpDexEntrypoint is LzApp {
     uint16 private constant _dstChainId = 0x0001;
+
+    constructor(address _lzEndpoint, address _owner) LzApp(_lzEndpoint) Ownable(_owner) {}
 
     function openPositionFor(
         address _vault,
@@ -15,36 +19,56 @@ contract PerpDexEntrypoint {
         uint256 _minDUSDCAmountOut,
         uint256 _deadline,
         bool _short
-    ) external onlyOwner {
+    ) external {
+        // Called by Vault
+        require(msg.sender == _vault, "PerpDexEntrypoint: not vault");
+
+        // Get collateral token (OFT)
         Vault vault = Vault(_vault);
         IOFTV2 collateral = IOFTV2(vault.underlying());
 
-        // encode the adapter parameters
-        uint16 version = 1;
-        uint256 gasForDestinationLzReceive = 350000;
-        bytes memory adapterParams = abi.encodePacked(version, gasForDestinationLzReceive);
+        bytes memory adapterParams = abi.encodePacked(uint16(1), uint256(350000));
 
         // encode the payload
         bytes memory payload = abi.encode(_vault, _receiver, _collateralAmountIn, _minDUSDCAmountOut, _deadline, _short);
 
+        // Estimate fee
         (uint256 nativeFee, uint256 zroFee) = collateral.estimateSendAndCallFee(
-            _dstChainId, bytes32(bytes20(address(this))), _collateralAmountIn, false, adapterParams
+            _dstChainId, bytes32(bytes20(address(this))), _collateralAmountIn, payload, 0, false, adapterParams
         );
 
-        // send the transaction
+        // Send OFT and message to PerpDexGateway for opening position
         collateral.sendAndCall(
-            collateral.owner(),
+            msg.sender,
             _dstChainId,
             bytes32(bytes20(address(this))),
             _collateralAmountIn,
             payload,
-            nativeFee,
-            IOFTV2.LzCallParams(address(0x0), address(0x0), adapterParams)
+            uint64(nativeFee),
+            ICommonOFT.LzCallParams(payable(msg.sender), address(0x0), adapterParams)
         );
     }
 
     function _blockingLzReceive(uint16 _srcChainId, bytes memory _srcAddress, uint64 _nonce, bytes memory _payload)
         internal
         override
-    {}
+    {
+        // Receive order result from PerpDexGateway
+        (
+            address vault,
+            bool success,
+            bool isShort,
+            address receiver,
+            uint256 excutedAmountOut,
+            uint256 orginAmountIn,
+            uint256 remainAmount,
+            uint256 excutedPrice,
+            uint256 excutedFee
+        ) = abi.decode(_payload, (address, bool, bool, address, uint256, uint256, uint256, uint256, uint256));
+
+        // Return result to Vault
+        Vault(vault).receivePerpOrder(
+            success, isShort, receiver, excutedAmountOut, orginAmountIn, remainAmount, excutedPrice, excutedFee
+        );
+    }
 }
